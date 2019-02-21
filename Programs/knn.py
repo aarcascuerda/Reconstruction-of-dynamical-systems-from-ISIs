ebaab89c94c6d4e444cba9f14d7305f869b7219b
Alex Arcas Cuerda <aarcascuerda@gmail.com>
2019-02-21 21:25:21 +0100

    knn done

--- Programs/knn.py
+++ Programs/knn.py
@@ -1,12 +1,16 @@
 import numpy as np 
-import scipy.spatial import distance 
+from scipy.spatial import distance 
 
 
 def neirest(vector_list):
     """vector_list must be of the type np.array([V0,V1,...,Vk]) with Vi = [ti,ti-1,ti-2]"""
     N = len(vector_list)
-    D = distance.squareform(distance.pdist(vector_list))
-    closest = np.argsort(D, axis=1)
-    v0_closest = closest[0][int(N*0.001):] #Note that we delete the 0.1% that would be interpolation.
+    D = distance.squareform(distance.pdist(vector_list))[0][int(N*0.001):] #Note that we delete the 0.1% that would be interpolation.
+    v0_closest = np.argsort(D) 
+    return v0_closest[:int(N*0.01)] #1% closest - torna una array amb els index dels mes propers en ordre.
+
+
+
     
+
 
